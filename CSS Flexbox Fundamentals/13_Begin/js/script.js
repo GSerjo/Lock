25e3d2dcb4e9d4b5ee1055b030258e421a3be1c7
GSerjo <sedot8@gmail.com>
2016-01-03 09:51:20 -0800

    + 013

---
+++ CSS Flexbox Fundamentals/13_Begin/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("#magnify").click(function(){
+        $(this).hide();
+        $("#search").toggle("scale");
+        $("#search").css("display", "block");
+    })
+});
+
