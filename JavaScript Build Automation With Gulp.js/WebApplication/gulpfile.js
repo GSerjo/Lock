12f0657889f2c4723e2edfd57aba9194b51d8cab
GSerjo <sedot8@gmail.com>
2016-01-23 23:22:32 -0800

    * with gulp-load-plugins

--- JavaScript Build Automation With Gulp.js/WebApplication/gulpfile.js
+++ JavaScript Build Automation With Gulp.js/WebApplication/gulpfile.js
@@ -1,28 +1,31 @@
 var gulp = require('gulp');
 
-var jshint = require('gulp-jshint');
-var jscs = require('gulp-jscs');
-var util = require('gulp-util');
-var gulpprint = require('gulp-print');
+//var jshint = require('gulp-jshint');
+//var jscs = require('gulp-jscs');
+//var util = require('gulp-util');
+//var gulpprint = require('gulp-print');
+
+var $ = require('gulp-load-plugins')({lazy: true});
+
 
 gulp.task('hello-world', function () {
     log('Analyzing source with JSHINT and JSCS');
 
     return gulp.src(['./ScriptsCustom/**/*.js'])
-               .pipe(gulpprint())
-               .pipe(jscs())
-               .pipe(jshint())
-               .pipe(jshint.reporter('jshint-stylish', {verbose: true}));
+               .pipe($.print())
+               .pipe($.jscs())
+               .pipe($.jshint())
+               .pipe($.jshint.reporter('jshint-stylish', { verbose: true }));
 });
 
 function log(message) {
     if (typeof (message) === 'object') {
         for (var item in message) {
             if (message.hasOwnProperty(item)) {
-                util.log(util.colors.blue(message[item]));
+                $.util.log($.util.colors.blue(message[item]));
             }
         }
     } else {
-        util.log(util.colors.blue(message));
+        $.util.log($.util.colors.blue(message));
     }
 }
