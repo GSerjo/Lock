755f59a27d884e2aff5e5cee9309980903cd9ec9
GSerjo <sedot8@gmail.com>
2016-01-23 23:13:05 -0800

    * gulp with print and log

--- JavaScript Build Automation With Gulp.js/WebApplication/gulpfile.js
+++ JavaScript Build Automation With Gulp.js/WebApplication/gulpfile.js
@@ -2,10 +2,27 @@ var gulp = require('gulp');
 
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var util = require('gulp-util');
+var gulpprint = require('gulp-print');
 
 gulp.task('hello-world', function () {
+    log('Analyzing source with JSHINT and JSCS');
+
     return gulp.src(['./ScriptsCustom/**/*.js'])
+               .pipe(gulpprint())
                .pipe(jscs())
                .pipe(jshint())
                .pipe(jshint.reporter('jshint-stylish', {verbose: true}));
 });
+
+function log(message) {
+    if (typeof (message) === 'object') {
+        for (var item in message) {
+            if (message.hasOwnProperty(item)) {
+                util.log(util.colors.blue(message[item]));
+            }
+        }
+    } else {
+        util.log(util.colors.blue(message));
+    }
+}
